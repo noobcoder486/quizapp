2b7f078e01918de91766fd03001a9de79cb19530
shubham <shubhamk3120@gmail.com>
2022-05-15 23:29:22 +0530

    quiz questions

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,4 @@
+from graphlib import TopologicalSorter
 from django.views.generic import ListView
 from django.contrib.auth.decorators import login_required
 from .models import Topic, Question, Answer, UserRecord
@@ -15,6 +16,15 @@ class TopicView(ListView):
     
 
 def nextques(request,t_id):
+    if request.method=="POST":
+        user=request.user
+        question_id=request.POST.get('hidden1')     
+        question=request.POST.get('hidden')
+        answer_id=request.POST.get(question)
+        topic_object=Topic.objects.get(t_id=t_id)
+        print(topic_object)
+        query=UserRecord(User=user,question=question_id,answer_choosen=answer_id,topic=topic_object)
+        query.save()
     questions=Question.objects.filter(topic=t_id)
     total_questions=[]
     for question in questions:
@@ -30,29 +40,11 @@ def nextques(request,t_id):
             break
         else:
             continue
-    context={
-        "questions":questionset,
-        "answers":answerset,
-    }
     if len(user_answered)<len(total_questions):
-        return render(request, "quiz/quizz.html", context=context)
+        return render(request, "quiz/quizz.html", context={"questions":questionset,"answers":answerset,})
     else:
         return render(request, "quiz/quizend.html")
 
-def savedata(request):
-    if request.method=="POST":
-        user=request.user
-        question_id=request.POST.get('hidden1')     
-        question=request.POST.get('hidden')
-        answer_id=request.POST.get(question)
-        question_object=Question.objects.filter(q_id=question_id)
-        topic=question_object.topic
-        topic_object=Topic.objects.filter(topic=topic)
-        topic_id=topic_object.t_id
-        print(topic_id)
-        query=UserRecord(User=user,question=question_id,answer_choosen=answer_id,topic=topic_object)
-        query.save()
-        return redirect(reverse('topic', kwargs={'args_1':topic_id}))
 
     
 
