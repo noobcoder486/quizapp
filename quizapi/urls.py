f8bcd93423ecb90a0adba8cf5dd81ead7b082381
Shubham Dubey <shubhamk3120@gmail.com>
2022-06-17 00:04:07 +0530

    Added correct answers in scoreview

--- quizapi/urls.py
+++ quizapi/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from .views import LoginAPIView, LogoutAPIView, QuestionAPIView, ResendOtpAPIView, ResultAPIView, ScoreAPIView, TopicAPIView, UserCreateAPIView, ValidateAPIView
 
 urlpatterns = [
-  path('login/', LoginAPIView.as_view(), name="login"),
-  path('logout/', LogoutAPIView.as_view(), name="logout"),
-  path('register/',UserCreateAPIView.as_view(), name='register'),
-  path('resend_otp', ResendOtpAPIView.as_view(), name="resend_otp"),
-  path("result/<uuid:topic_id>", ResultAPIView.as_view(), name = "result"),
-  path('score/<uuid:topic_id>', ScoreAPIView.as_view(), name='score'),
+  path('login/', LoginAPIView.as_view(), name="api_login"),
+  path('logout/', LogoutAPIView.as_view(), name="api_logout"),
+  path('register/',UserCreateAPIView.as_view(), name='api_register'),
+  path('resend_otp', ResendOtpAPIView.as_view(), name="api_resend_otp"),
+  path("result/<uuid:topic_id>", ResultAPIView.as_view(), name = "api_result"),
+  path('score/<uuid:topic_id>', ScoreAPIView.as_view(), name='api_score'),
   path('topic/', TopicAPIView.as_view(), name= 'topics'),
-  path('<uuid:topic_id>', QuestionAPIView.as_view(), name='question'),
-  path('validate_otp', ValidateAPIView.as_view(), name='validate'),
+  path('<uuid:topic_id>', QuestionAPIView.as_view(), name='api_question'),
+  path('validate_otp', ValidateAPIView.as_view(), name='api_validate'),
 ]
